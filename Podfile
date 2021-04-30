cdd0c01a6a7da0df296c9d4ac741a86896b6601e
youn2029 <cocoa8090@gmail.com>
2021-04-30 20:22:21 +0900

    tableView Cell setting

--- Podfile
+++ Podfile
@@ -5,8 +5,9 @@ target 'RxCocoa_UITest' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-    pod 'RxSwift', '6.1.0'
-    pod 'RxCocoa', '6.1.0'
+pod 'RxSwift', '6.1.0'
+pod 'RxCocoa', '6.1.0'
+pod 'RxDataSources'
 
   # Pods for RxCocoa_UITest
 
--- RxCocoa_UITest/SecondViewController.swift
+++ RxCocoa_UITest/SecondViewController.swift
@@ -6,33 +6,37 @@
 //
 
 import UIKit
+import RxDataSources
 
 class SecondViewController: UIViewController {
     
+    /*
+     참고 사이트 : https://github.com/RxSwiftCommunity/RxDataSources
+     */
+    
     let disposeBag = DisposeBag()
+    let names = ["첫번째", "두번째", "세번째", "네번째", "다섯번째", "여섯번째", "일곱번째", "여덟번째", "아홉번째", "열번째"]
+    lazy var namesOb: Observable<[String]> = Observable.of(names)
     
     @IBOutlet weak var testTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bindTableView()
-        // Do any additional setup after loading the view.
+//        bindTableView_01()
+//        bindTableView_02()
+        bindTableView_03()
+        
     }
     
-    func bindTableView() {
-        print("bind TableView")
-        
-        let names = ["첫번째", "두번째", "세번째", "네번째", "다섯번째", "여섯번째", "일곱번째", "여덟번째", "아홉번째", "열번째"]
-        let namesOb: Observable<[String]> = Observable.of(names)
+    // UITableViewCell를 이용하여 cell 설정 1 - UITableView.rx.items(_:_:)
+    func bindTableView_01() {
         
         namesOb.bind(to: testTableView.rx.items) {
             (tableView: UITableView, index: Int, element: String) -> UITableViewCell in
 
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell") else {
-                let cell = UITableViewCell()
-                cell.textLabel?.text = element
-                return cell
+                return UITableViewCell()
             }
 
             cell.textLabel?.text = element
@@ -41,5 +45,37 @@ class SecondViewController: UIViewController {
 
         }.disposed(by: disposeBag)
     }
+    
+    // UITableViewCell를 이용하여 cell 설정 2 - UITableView.rx.items(cellIdentifier:String)
+    func bindTableView_02() {
+        
+        namesOb.bind(to: testTableView.rx.items(cellIdentifier: "tableCell")) { (index, model, cell:UITableViewCell) in
+            cell.textLabel?.text = model
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    // customCell을 tableViewCell로 설정 - UITableView.rx.items(cellIdentifier:String:Cell.Type:_:)
+    func bindTableView_03() {
+        
+        namesOb.bind(to: testTableView.rx.items(cellIdentifier: "tableCell", cellType: BasicTableCell.self)) { (index, model, cell) in
+            cell.lblTitle.text = model
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    // RxDataSource를 이용하여 cell 설정 - UITableView.rx.items(dataSource:protocol<RxTableViewDataSourceType, UITableViewDataSource>)
+    func bindTableView_04() {
+        
+    }
+}
 
+class BasicTableCell: UITableViewCell {
+    
+    @IBOutlet weak var lblTitle: UILabel!
+
+}
+
+open class RxTableViewSectionedReloadDataSource<S: SectionModelType> : TableViewSectionedDataSource<S> {
+    
 }
